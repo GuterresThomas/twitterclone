898bade0b18476cb8cdf88220a04dff0291a7f39
Guterresthomas <guterresmikael@gmail.com>
2023-07-10 21:07:06 -0300

    att responsiva

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'mobile': '300px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
